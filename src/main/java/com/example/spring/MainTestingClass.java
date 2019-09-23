cb2ec20e2a1f01704dff208a430d9214ec307602
Wick Code <wick74860@gmail.com>
2019-09-24 00:02:48 +0530

    Providing objects through XML config

--- src/main/java/com/example/spring/MainTestingClass.java
+++ src/main/java/com/example/spring/MainTestingClass.java
@@ -1,13 +1,15 @@
 package com.example.spring;
 
 import com.example.spring.implementation.BraveKnight;
-import com.example.spring.implementation.DragonSlayerQuest;
 import com.example.spring.service.Knight;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainTestingClass {
 
     public static void main(String[] args){
-        Knight knight = new BraveKnight(new DragonSlayerQuest());
+        ApplicationContext context = new ClassPathXmlApplicationContext("Knights.xml");
+        Knight knight = (BraveKnight) context.getBean("braveKnight");
         knight.embarkOnQuest();
     }
 }
